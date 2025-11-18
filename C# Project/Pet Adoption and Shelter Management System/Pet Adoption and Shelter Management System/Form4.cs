49e5f95bc0067463fde7db86bc0680113a9925ed
hl <104311210+hle0110@users.noreply.github.com>
2025-11-18 11:51:14 -0500

    Form4.cs

--- C# Project/Pet Adoption and Shelter Management System/Pet Adoption and Shelter Management System/Form4.cs
+++ C# Project/Pet Adoption and Shelter Management System/Pet Adoption and Shelter Management System/Form4.cs
@@ -17,9 +17,54 @@ namespace Pet_Adoption_and_Shelter_Management_System
             InitializeComponent();
         }
 
-        private void label5_Click(object sender, EventArgs e)
+        private void BtnAdd_Click(object sender, EventArgs e)
         {
-
+            if (TxtName.Text != "")
+            {
+                string pet = TxtName.Text + " - " + CBoxSpecies.Text + " - " + TxtBreed.Text;
+                LBoxView.Items.Add(pet);
+                MessageBox.Show("Pet added!");
+                TxtName.Clear();
+            }
         }
+
+        private void BtnEdit_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Edit - Coming Soon!");
+        }
+
+        private void BtnDelete_Click(object sender, EventArgs e)
+        {
+            if (LBoxView.SelectedIndex != -1)
+            {
+                LBoxView.Items.RemoveAt(LBoxView.SelectedIndex);
+                MessageBox.Show("Pet deleted!");
+            }
+        }
+
+        private void BtnClear_Click(object sender, EventArgs e)
+        {
+            TxtName.Clear();
+            TxtAge.Clear();
+            TxtBreed.Clear();
+        }
+
+        private void label5_Click(object sender, EventArgs e) { }
+        private void TxtName_TextChanged(object sender, EventArgs e) { }
+        private void LblName_Click(object sender, EventArgs e) { }
+        private void TxtAge_TextChanged(object sender, EventArgs e) { }
+        private void LblAge_Click(object sender, EventArgs e) { }
+        private void LblSpecies_Click(object sender, EventArgs e) { }
+        private void CBoxSpecies_SelectedIndexChanged(object sender, EventArgs e) { }
+        private void LblLocation_Click(object sender, EventArgs e) { }
+        private void CBoxLocation_SelectedIndexChanged(object sender, EventArgs e) { }
+        private void label1_Click(object sender, EventArgs e) { }
+        private void TxtBreed_TextChanged(object sender, EventArgs e) { }
+        private void LblGender_Click(object sender, EventArgs e) { }
+        private void GBoxSex_Enter(object sender, EventArgs e) { }
+        private void LblD_Click(object sender, EventArgs e) { }
+        private void TxtD_TextChanged(object sender, EventArgs e) { }
+        private void LBoxView_SelectedIndexChanged(object sender, EventArgs e) { }
+        private void LblView_Click(object sender, EventArgs e) { }
     }
 }
