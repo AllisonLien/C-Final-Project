a4cb7dae78ecde1b675b40aacf3976e4fe5d4845
hl <104311210+hle0110@users.noreply.github.com>
2025-11-18 03:12:35 -0500

    Add files via upload

---
+++ C# Project/Pet Adoption and Shelter Management System/Pet Adoption and Shelter Management System/obj/UserSession.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pet_Adoption_and_Shelter_Management_System
+{
+    public static class UserSession
+    {
+        public static int UserId { get; set; }
+        public static string Username { get; set; }
+        public static string Role { get; set; }
+
+        public static void SetUser(int userId, string username, string role)
+        {
+            UserId = userId;
+            Username = username;
+            Role = role;
+        }
+
+        public static void Clear()
+        {
+            UserId = 0;
+            Username = null;
+            Role = null;
+        }
+    }
+}
