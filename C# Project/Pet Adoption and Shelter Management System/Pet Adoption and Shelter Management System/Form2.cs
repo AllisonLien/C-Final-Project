4a2fa7b2f9714d73b523a44a8f21facbec167289
hl <104311210+hle0110@users.noreply.github.com>
2025-11-18 03:29:52 -0500

    Form2.cs
    Added password masking and validation for registration.

--- C# Project/Pet Adoption and Shelter Management System/Pet Adoption and Shelter Management System/Form2.cs
+++ C# Project/Pet Adoption and Shelter Management System/Pet Adoption and Shelter Management System/Form2.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +15,50 @@ namespace Pet_Adoption_and_Shelter_Management_System
         public FormRegister()
         {
             InitializeComponent();
+            TxtPass.PasswordChar = '*';
+            TxtCpass.PasswordChar = '*';
+            ComRole.SelectedIndex = 0;
         }
+
+        private void BtnRegister_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(TxtName.Text))
+            {
+                MessageBox.Show("Enter username!");
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(TxtPass.Text))
+            {
+                MessageBox.Show("Enter password!");
+                return;
+            }
+
+            if (TxtPass.Text != TxtCpass.Text)
+            {
+                MessageBox.Show("Passwords don't match!");
+                return;
+            }
+
+            MessageBox.Show("Registration Successful!");
+            this.Close();
+        }
+
+        private void BtnClear_Click(object sender, EventArgs e)
+        {
+            TxtName.Clear();
+            TxtPass.Clear();
+            TxtCpass.Clear();
+        }
+
+        private void label1_Click(object sender, EventArgs e) { }
+        private void LblName_Click(object sender, EventArgs e) { }
+        private void TxtName_TextChanged(object sender, EventArgs e) { }
+        private void FormRegister_Load(object sender, EventArgs e) { }
+        private void TxtPass_TextChanged(object sender, EventArgs e) { }
+        private void LblCpass_Click(object sender, EventArgs e) { }
+        private void TxtCpass_TextChanged(object sender, EventArgs e) { }
+        private void LblRole_Click(object sender, EventArgs e) { }
+        private void ComRole_SelectedIndexChanged(object sender, EventArgs e) { }
     }
 }
